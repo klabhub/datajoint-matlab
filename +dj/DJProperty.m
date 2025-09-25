3e7835f5ae2d9801768d9fca2f59f6559b6e3eb4
Mert Özkan <mert.ozkan@rutgers.edu>
2025-09-25 16:39:55 -0400

    Update DJProperty.m
    Working on changin property logic but it is not functional yet

--- +dj/DJProperty.m
+++ +dj/DJProperty.m
@@ -1,26 +1,20 @@
-classdef (Abstract) DJProperty < handle
+classdef DJProperty < handle
     %% Property caller for datajoint instances
     %
-
-    properties
-        
-        value
-    end
-    properties (Abstract, Access = protected)
+    properties (Access = protected)
                
-        parent % parent datajoint class
+        parent dj.DJInstance% parent datajoint class
+        identity_vars_
+        value = []
 
-    end
-
-    properties (Abstract, Access =protected, Constant)
-        identity_vars cell % if multiple rows have the same values for 
-        % these columns the property associated with these rows are 
-        % expected to be the same.
     end
 
     properties (Dependent)
         
         n_rows
+        identity_vars % if multiple rows have the same values for 
+        % these columns the property associated with these rows are 
+        % expected to be the same. comes from the parent
         
     end
 
@@ -32,10 +26,54 @@ classdef (Abstract) DJProperty < handle
 
     methods
 
+        function self = DJProperty(parent, prop_name, pv)
+
+            arguments
+                
+                parent dj.DJInstance
+                prop_name {mustBeText}
+
+                % Use the following options when the property has different
+                % 
+                pv.add_identity_var (1,:) cell = {}
+                pv.remove_identity_var (1,:) cell = {}
+                pv.set_identity_var (1,:) cell = {}
+
+            end
+
+            self.parent = parent;
+
+            if ~isempty(pv.set_identity_var)
+
+                self.identity_vars = self.parent.primaryKey;
+            
+            else
+
+                self.identity_vars = pv.set_identity_var;
+
+            end
+
+            if ~isempty(pv.add_identity_var)
+
+                self.identity_vars = [self.identity_vars, pv.add_identity_var];
+
+            end
+
+            if ~isempty(pv.remove_identity_var)
+
+                self.identity_vars = setdiff(self.identity_vars, pv.remove_identity_var);
+                
+            end
+            addlistener(parent, prop_name, 'PreGet', self.make());
+
+
+        end
+
         function self = make(self)
             
             % method to call properties per row
             % get_method must have been defined within the instance class
+            if ~isempty(self.value), return; end
 
             tbl = self.parent;
             tpls = fetchtable(self.parent,self.identity_vars{:});
@@ -67,6 +105,18 @@ classdef (Abstract) DJProperty < handle
 
         end
 
+        function vars = get.identity_vars(self)
+
+            vars = self.identity_vars_;
+
+        end
+
+        function set.identity_vars(self, val)
+            % if the property has different identity variable set than the
+            % parent, you can overwrite
+            self.identity_vars_ = val;
+            
+        end
         
         function parent = get_parent(self)
 
@@ -83,6 +133,8 @@ classdef (Abstract) DJProperty < handle
                 
             end
         end
+
+        
 
     end
 
